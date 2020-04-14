cca2f18e1633d80ecd4d9ea8afcd57c294688856
Yuxuan Fan <39680169+991231@users.noreply.github.com>
2020-04-13 23:55:47 -0700

    PA03

---
+++ PA03/Message.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+
+public abstract class Message {
+
+    // Use these variable names as the msgType argument of sendMessage()
+    // DO NOT MODIFY!
+    public static final int TEXT    = 1000;
+    public static final int PHOTO   = 1001;
+    public static final int STICKER = 1002;
+
+    // Error message to use in OperationDeniedException
+    protected static final String DENIED_USER_GROUP =
+            "This operation is disabled in your user group.";
+
+    // instance variables
+    private LocalDate date;
+    private User sender;
+    protected String contents;
+
+    public Message(User sender) {
+        this.date = LocalDate.now();
+        /* TODO */
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public User getSender() {
+        /* TODO */
+        return null;
+    }
+
+    public abstract String getContents();
+
+}
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This is the starter code repository for DSC 30 Spring 2020. For information abou
 | - | ----- | --------- | -------- | ------------ |
 | 01 | Introduction to Java | 4/6 Monday 11:59 pm | [Google Docs](https://docs.google.com/document/d/1S8AYut60gYqrfl3ijKRqFJpb-u-ccvrWwpq9vy0ejx4/edit?usp=sharing) | N/A |
 | 02 | Stack and JUnit | 4/13 Monday 11:59 pm | [Google Docs](https://docs.google.com/document/d/1ELqPRjz5ZOZZSr7719ZuA5GxeGXZ8wV6jOnb7Nz6i8s/edit?usp=sharing) | [PA02 folder](https://github.com/ucsd-ets/dsc30sp20-startercode/tree/master/PA02) |
-| 03 |  |  |  |
+| 03 | Object-oriented Programming | 4/20 Monday 11:59 pm | [Google Docs](https://docs.google.com/document/d/1Mmb1hZEPJeHmtYKI7Sn8yvcuzIgyMj7IVs_Z0TJRv18/edit?usp=sharing) | [PA03 folder](https://github.com/ucsd-ets/dsc30sp20-startercode/tree/master/PA03) |
 | 04 |  |  |  |
 | 05 |  |  |  |
 | 06 |  |  |  |
